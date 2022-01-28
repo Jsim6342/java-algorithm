f23b57b4e7570d3f97ba6ac22b9a7d603de185c9
Jsim6342 <snowdrop6342@gmail.com>
2022-01-28 18:46:38 +0900

    2022.01.28일자 알고리즘 풀이

--- programmers/_18/Main.java
+++ programmers/_18/Main.java
@@ -4,56 +4,29 @@ import java.util.*;
 
 // 수식 최대화(틀린 풀이)
 class Main {
-    static ArrayList<String> nowOperations = new ArrayList<>();
-    static String[] operationTypes = {"-","+","*"};
-    static ArrayList<ArrayList<String>> operatorCases = new ArrayList<>();
-    static ArrayList<Long> answers = new ArrayList<>();
+    static char[] prior = {'+', '-', '*'};
+    static boolean[] check = new boolean[3];
+    static ArrayList<Long> nums = new ArrayList<Long>();
+    static ArrayList<Character> ops = new ArrayList<Character>();
+    static long answer;
 
     public long solution(String expression) {
+        answer = 0;
 
-        Deque<Long> numbers = new ArrayDeque<>();
-        Deque<String> operators = new ArrayDeque<>();
-
-
-        String[] splitNums = expression.split("-|\\*|\\+");
-        for (String stringNum : splitNums) {
-            numbers.add(Long.valueOf(stringNum));
-        }
-        String[] splitOperators = expression.split("[0-9]");
-        for (String operator : splitOperators) {
-            operators.add(operator);
-        }
-
-
-        getOperatorCases(0);
-
-
-
-        for (ArrayList<String> operatorCase : operatorCases) {
-            //solve(operatorCase, numbers, operators);
-        }
-
-        for (Long answer : answers) {
-            System.out.println(answer);
-        }
-        return 0L;
-    }
-
-    public static void solve(ArrayList<String> operatorCase, Deque<Long> numbers, Deque<String> operators) {
-        Deque<Long> nextNumbers = new ArrayDeque<>();
-        nextNumbers.add(numbers.pollFirst());
-
-        for (String nowOperator : operatorCase) {
-            for (String operator: operators) {
-                if(nowOperator == operator) {
-                    Long result = calc(nextNumbers.getLast(), numbers.pollFirst(), operator.charAt(0));
-                    nextNumbers.add(result);
-                }else{
-                    nextNumbers.add(numbers.pollFirst());
-                }
+        String num="";
+        for(int i=0;i<expression.length();i++){
+            if(expression.charAt(i) >= '0' && expression.charAt(i) <= '9'){
+                num += expression.charAt(i);
+            }else{
+                nums.add(Long.parseLong(num));
+                num = "";
+                ops.add(expression.charAt(i));
             }
         }
-        answers.add(Math.abs(nextNumbers.getLast()));
+        nums.add(Long.parseLong(num));
+        dfs(0, new char[3]);
+
+        return answer;
     }
 
     public static Long calc(Long num1, Long num2, char op){
@@ -72,21 +45,32 @@ class Main {
         return num;
     }
 
-    public void getOperatorCases(int level) {
-        if(level == operationTypes.length) {
-            ArrayList<String> temp = new ArrayList<>();
-            for (String nowOperation : nowOperations) {
-                temp.add(nowOperation);
-            }
-            operatorCases.add(temp);
-        }else {
-            for (String operationType : operationTypes) {
-                if(nowOperations.contains(operationType)) continue;
-                else {
-                    nowOperations.add(operationType);
-                    getOperatorCases(level + 1);
-                    nowOperations.remove(operationType);
+    public static void dfs(int count, char[] p){
+        if(count == 3){
+            ArrayList<Long> cNums = new ArrayList<>(nums);
+            ArrayList<Character> cOps = new ArrayList<Character>(ops);
+
+            for(int i=0;i<p.length;i++){
+                for(int j=0; j< cOps.size(); j++){
+                    if(p[i] == cOps.get(j)){
+                        Long res = calc(cNums.remove(j), cNums.remove(j), p[i]);
+                        cNums.add(j, res);
+                        cOps.remove(j);
+                        j--;
+                    }
                 }
+            }
+            answer = Math.max(answer, Math.abs(cNums.get(0)));
+            return;
+
+        }
+
+        for(int i=0; i< 3; i++){
+            if(!check[i]){
+                check[i] = true;
+                p[count] = prior[i];
+                dfs(count+1,p);
+                check[i] = false;
             }
         }
     }
